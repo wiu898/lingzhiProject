01ab6604e0d6ada15d69ea20e10d6a428cb56cb2
lord <1203967359@qq.com>
2016-01-11 10:01:32 +0800

    初始化项目

---
+++ lingzhiProject/lingzhiProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lingzhiProject
+//
+//  Created by bestseller on 16/1/11.
+//  Copyright © 2016年 lord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lingzhiProject/lingzhiProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lingzhiProject
+//
+//  Created by bestseller on 16/1/11.
+//  Copyright © 2016年 lord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
